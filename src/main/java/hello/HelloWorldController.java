9adefaac7890e9a1c8da8c4caae9fba203725eeb
bangshinchul <bsc0227@naver.com>
2018-08-02 01:19:52 +0900

    180802-2 commit

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -20,6 +20,6 @@ public class HelloWorldController {
     @ResponseBody
     public String defaultHello(@RequestParam(name="name", required=false, defaultValue = "Stranger") String name) {
 //        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-        return "안녕하세요! 해당 RESTful api는 0.1.8 버전이며, Spring Boot Gradle로 제작하였습니다. 배포는 AWS CodeDeploy & Travis CI로 자동화 하였습니다. 18.07.28";
+        return "안녕하세요! 해당 RESTful api는 0.1.8 버전이며, Spring Boot Gradle로 제작하였습니다. 배포는 AWS CodeDeploy & Travis CI로 자동화 하였습니다. 현재 프론트엔드는 s3에 배포되어 있습니다. 18.08.02";
     }
 }
