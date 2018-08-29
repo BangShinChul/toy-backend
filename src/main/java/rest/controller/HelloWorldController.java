2aefb48d4fe91a43ceb030a903b4ea343bf53127
bangshinchul <bsc0227@naver.com>
2018-08-30 01:26:57 +0900

    180830-2 commit

--- src/main/java/rest/controller/HelloWorldController.java
+++ src/main/java/rest/controller/HelloWorldController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-@CrossOrigin(origins = {"http://localhost:9000","http://localhost:9001","http://localhost:3000","http://localhost:80","http://localhost:8080","http://bangshinchul.com/#/ApiTest", "http://bangshinchul.com", "http://rest.bangshinchul.com:9000"}, maxAge = 4800, allowCredentials = "false")
+@CrossOrigin(origins = {"*"}, maxAge = 4800, allowCredentials = "false")
 @RestController
 @RequestMapping("/main")
 public class HelloWorldController {
@@ -22,6 +22,6 @@ public class HelloWorldController {
     @ResponseBody
     public String defaultHello(@RequestParam(name="name", required=false, defaultValue = "Stranger") String name) {
 //        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-        return "안녕하세요! 해당 RESTful api는 0.1.8 버전이며, Spring Boot Gradle로 제작하였습니다. 배포는 AWS CodeDeploy & Travis CI로 자동화 하였습니다. 현재 프론트엔드는 s3에 배포되어 있습니다. 18.08.29";
+        return "안녕하세요! 해당 RESTful api는 0.1.8 버전이며, Spring Boot Gradle로 제작하였습니다. 배포는 AWS CodeDeploy & Travis CI로 자동화 하였습니다. 현재 프론트엔드는 s3에 배포되어 있습니다. 18.08.30";
     }
 }
